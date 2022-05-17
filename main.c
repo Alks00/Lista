d13df0144b09747e329223e597c11535b6bccdf5
Lucas Szczepaniak <szczelucas@hotmail.com>
2022-05-17 20:34:32 -0300

    menu

--- main.c
+++ main.c
@@ -26,6 +26,49 @@ void main(){
         case 1:
             Lista* l = criar_lista();
             break;
+        case 2:
+            printf("Digite um nome:\n");
+            scanf("%s",&nome);
+            printf("Digite um valor:\n");
+            scanf("%d",&num);
+
+            adicionar_elemento(l,nome,num);
+
+            break;
+        case 3:
+            printf("Digite um nome:\n");
+            scanf("%s",&nome);
+            printf("Digite um valor:\n");
+            scanf("%d",&num);
+
+            adicionar_inicio(l,nome,num);
+
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            remover_inicio(l);
+            break;
+        case 7:
+            printf("Digite a posição a ser removida:\n");
+            scanf("%d",&pos);
+            remover_pos(l,pos);
+            break;
+        case 8:
+            print_lista(l);
+            break;
+        case 9:
+            if(lista_vazia(l)){
+                printf("Lista vazia");
+            }else printf("A lista possui %d elementos.",tamanho_lista(l));
+            break;
+        case 10:
+            finalizar_lista(l);
+            break;
+        case 11:
+            break;
         
         default:
             break;
