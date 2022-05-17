c46819a2244ab3c5e8515abcb939fde4d68af3b3
Lucas Szczepaniak <szczelucas@hotmail.com>
2022-05-17 15:57:55 -0300

    testes

--- ListaCircular.h
+++ ListaCircular.h
@@ -20,3 +20,17 @@ int lista_vazia(Lista* lista);
 
 void adicionar_elemento(Lista* lista,char nome,int n);
 
+void print_lista(Lista* lista);
+
+void remover_pos(Lista* lista,int pos);
+
+void adicionar_inicio(Lista* lista,char nome,int n);
+
+void remover_inicio(Lista* lista);
+
+int tamanho_lista(Lista* lista);
+
+void finalizar_lista(Lista* lista);
+
+
+
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "ListaCircular.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    
+
+    return;
+}
